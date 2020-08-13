a9f3c455a4a1b09c168ccafe0d8c4b4d615b40ad
chaosannals <chaosannals@outlook.com>
2020-08-13 17:13:05 +0800

    demo

--- python/app.py
+++ python/app.py
@@ -1,8 +1,18 @@
+import random
 from exert.indexer import Indexer
+from exert.vgg16 import VGG16Extractor
 
 indexer = Indexer('exert')
+extractor = VGG16Extractor()
 try:
     indexer.init(True)
-    
+    v = extractor.extract('asset/test.jpg')
+    ids = indexer.index([v])
+    print('{} => {}'.format(len(v), ids))
+    indexer.flush()
+    c = indexer.search([v])
+    print(c)
+    indexer.unindex([i.id for i in c[0]])
+    indexer.close()
 except Exception as e:
     print(e)
